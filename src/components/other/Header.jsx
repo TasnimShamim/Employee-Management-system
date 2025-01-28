ae8f26af807466a9485490cc9bbab5e34e16a82e
TasnimShamim <tasnimshamim35@gmail.com>
2025-01-28 19:43:06 +0530

    typo changed

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='flex items-end justify-between'>
-        <h1 className='text-xl font-medium' >Hello <br/> <span className='text-2xl font-semibold'>Tasnim👋</span> </h1>
+        <h1 className='text-xl font-medium' >Hello <br/> <span className='text-2xl font-semibold'>Tasnim Shamim👋</span> </h1>
         <button className='bg-red-600 text-white px-4 py-2 rounded-sm text-lg font-medium'>Log Out</button>
     </div>
   )
