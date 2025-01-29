345af8b6269cc37af09627ec014ca0408602b215
TasnimShamim <tasnimshamim35@gmail.com>
2025-01-29 16:29:00 +0530

    added task

---
+++ src/components/other/AllTask.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5  rounded mt-5 h-54 overflow-auto'>
+        <div className=' bg-teal-700 rounded  mb-2 py-2 px-4 flex justify-between'>
+            <h2>Tasnim</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-blue-500  mb-2 rounded  py-2 px-4 flex justify-between'>
+            <h2>Tasnim</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-yellow-400 mb-2 rounded  py-2 px-4 flex justify-between'>
+            <h2>Tasnim</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-violet-400 mb-2 rounded  py-2 px-4 flex justify-between'>
+            <h2>Tasnim</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className=' bg-pink-300 rounded  mb-2 py-2 px-4 flex justify-between'>
+            <h2>Tasnim</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>           
+        </div>
+        <div className=' bg-sky-950 rounded  mb-2 py-2 px-4 flex justify-between'>
+            <h2>Tasnim</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>           
+        </div>
+        <div className=' bg-green-500 rounded  mb-2 py-2 px-4 flex justify-between'>
+            <h2>Tasnim</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>           
+        </div>
+        <div className=' bg-red-400 rounded  mb-2 py-2 px-4 flex justify-between'>
+            <h2>Tasnim</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>           
+        </div>
+    </div>
+  )
+}
+
+export default AllTask
