04b729827abd7d518472bead10deff8f82d402f1
TasnimShamim <tasnimshamim35@gmail.com>
2025-02-20 14:18:17 +0530

    adding data

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../context/AuthProvider";
 
-const AllTask = () => {
-  const authData = useContext(AuthContext);
+const AllTask = () =>{
+  const [userData,setUserData] = useContext(AuthContext);
   // console.log(authData.employees)
   return (
     <div className="bg-[#1c1c1c] p-5  rounded mt-5 h-69 ">
@@ -14,7 +14,7 @@ const AllTask = () => {
         <h5 className="w-1/5">Failed</h5>
       </div>
       <div className="h-[80%] overflow-auto">
-        {authData.employees.map(function(elem,idx){
+        {userData.employees.map(function(elem,idx){
           return <div key={idx} className="border-2 border-slate-500 rounded mb-2 py-2 px-4 flex justify-between">
               <h3 className=" text-lg font-medium w-1/5 !text-blue-400">{elem.firstName}</h3>
               <h2 className=" text-lg font-medium w-1/5">{elem.taskCounts.newTask}</h2>
